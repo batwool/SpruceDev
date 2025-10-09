af0dd03ac0c7edb2336893d3f3a1e7a90070caff
JacopoFB <jacopofb03@gmail.com>
2025-10-09 11:30:52 -0600

    styling and icon work

--- assets/components/CTA.js
+++ assets/components/CTA.js
@@ -1,12 +1,12 @@
 class SiteCTA extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-            <section class="dark-gradient-section" id="contact">
+            <section class="dark-section" id="contact">
                 <div class="container">
                     <div class="section-title">
                         <h2>Ready to Grow Your Business?</h2>
                         <p>Contact us today for a free consultation and estimate. Let's discuss how we can help your business thrive online.</p>
-                        <a href="#contact" class="cta-button mtop50 inline-block">Get Your Free Estimate</a>
+                        <a href="#contact" class="dark-cta-button mtop50 inline-block">Get Your Free Estimate</a>
                     </div>
                 </div>
             </section>`;
